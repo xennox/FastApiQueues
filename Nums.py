9a53af99a9cad50c22f94f1986a5f49be851493f
Xen0x <61052112+xennox@users.noreply.github.com>
2022-08-05 10:00:18 +0300

    added automatic async endpoint for parsing tasks

--- Nums.py
+++ Nums.py
@@ -1,13 +1,23 @@
 import uvicorn
-import time
+import asyncio
 from datetime import datetime
 from fastapi import FastAPI, status
+from fastapi_utils.tasks import repeat_every
 
 
 app = FastAPI(title='WorkerAPI')
 
 NumData = []
 WorksData = []
+
+
+@app.on_event("startup")
+@repeat_every(seconds=10)
+async def createfromworker() -> None:
+    for a in WorksData:
+        NumData.append(a['Num'])
+        WorksData.pop(0)
+        print(NumData)
 
 
 @app.get('/getnums', status_code=200, tags=['NUMS'])
@@ -20,12 +30,12 @@ async def show():
     return WorksData
 
 
-@app.post('/makenum', status_code=status.HTTP_201_CREATED, tags=['NUMS'])
-async def create():
-    for a in WorksData:
-        NumData.append(a['Num'])
-        WorksData.pop(0)
-    return NumData
+#@app.post('/makenum', status_code=status.HTTP_201_CREATED, tags=['NUMS'])
+#async def create():
+#    for a in WorksData:
+#        NumData.append(a['Num'])
+#        WorksData.pop(0)
+#    return NumData
 
 
 @app.post('/makework', status_code=status.HTTP_201_CREATED, tags=['WORKS'])
@@ -33,12 +43,13 @@ async def create(num: int, timeout: int):
     historynum = len(WorksData) + 1
     createdtime = datetime.now()
     alldata = {'HistoryNum': historynum, 'CreatedTime': str(createdtime), 'Num': num, 'TimeOut': timeout}
+    await asyncio.sleep(timeout)
     WorksData.append(alldata)
-    time.sleep(timeout)
 
     if historynum > 20:
         WorksData.pop(0)
     return WorksData
+
 
 if __name__ == '__main__':
     uvicorn.run(app, host='127.0.0.1', port=8000)
